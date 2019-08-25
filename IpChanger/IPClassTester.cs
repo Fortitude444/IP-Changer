4657b9020d60a4a62856e1aba8ec195b48290318
Márk Molnár <markmolnar1996@gmail.com>
2019-08-25 14:05:32 +0200

    Raise IPClassTester from MainWindow

--- IpChanger/IPClassTester.cs
+++ IpChanger/IPClassTester.cs
@@ -2,56 +2,52 @@
 
 namespace IpChanger
 {
-    public partial class MainWindow
+    class IPClassTester
     {
-        class IPClassTester
+        public static string GetSubnetMask(string ipaddress)
+        {
+            uint firstOctet = ReturnFirtsOctet(ipaddress);
+            if (firstOctet >= 0 && firstOctet <= 127 && firstOctet != 0)
+                return "255.0.0.0";
+
+            else if (firstOctet >= 128 && firstOctet <= 191)
+                return "255.255.0.0";
+
+            else if (firstOctet >= 192 && firstOctet <= 223)
+                return "255.255.255.0";
+
+            else if (ipaddress.Contains(".") == false)
+                return "";
+
+            else return "0.0.0.0";
+        }
+
+        public static string GatewayAutoComplete(string ipaddress)
+        {
+            if (ipaddress.Contains(".") == true)
+            {
+                string gateWayAddress = ipaddress.Substring(0, ipaddress.LastIndexOf("."));
+                return gateWayAddress + ".1";
+            }
+            else
+                return "";
+        }
+
+        public static uint ReturnFirtsOctet(string ipAddress)
 
         {
-            public static string GetSubnetMask(string ipaddress)
+            IPAddress IP;
+            bool flag = IPAddress.TryParse(ipAddress, out IP);
+            if (flag == true)
             {
-                uint firstOctet = ReturnFirtsOctet(ipaddress);
-                if (firstOctet >= 0 && firstOctet <= 127 && firstOctet != 0)
-                    return "255.0.0.0";
+                IP = IPAddress.Parse(ipAddress);
+                byte[] byteIP = IP.GetAddressBytes();
+                uint ipInUint = (uint)byteIP[0];
 
-                else if (firstOctet >= 128 && firstOctet <= 191)
-                    return "255.255.0.0";
-
-                else if (firstOctet >= 192 && firstOctet <= 223)
-                    return "255.255.255.0";
-
-                else if (ipaddress.Contains(".") == false)
-                    return "";
-
-                else return "0.0.0.0";
+                return ipInUint;
             }
-
-            public static string GatewayAutoComplete(string ipaddress)
-            {
-                if (ipaddress.Contains(".") == true)
-                {
-                    string gateWayAddress = ipaddress.Substring(0, ipaddress.LastIndexOf("."));
-                    return gateWayAddress + ".1";
-                }
-                else
-                    return "";
-            }
-
-            public static uint ReturnFirtsOctet(string ipAddress)
-
-            {
-                IPAddress IP;
-                bool flag = IPAddress.TryParse(ipAddress, out IP);
-                if (flag == true)
-                {
-                    IP = IPAddress.Parse(ipAddress);
-                    byte[] byteIP = IP.GetAddressBytes();
-                    uint ipInUint = (uint)byteIP[0];
-
-                    return ipInUint;
-                }
-                else
-                    return 0;
-            }
+            else
+                return 0;
         }
     }
 }
